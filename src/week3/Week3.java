667788f13cc3d7eb3ba0d509ce491c0b76816d70
truongphat17020948 <truongphat0799@gmail.com>
2018-10-02 23:26:26 +0700

    update3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,25 +1,43 @@
 package week3;
 
 public class Week3 {
-
+    int a;
     public static int max(int m, int n) {
+        if(m>n) return m;
+        else return n;
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
     }
 
     public static int minOfArray(int[] array) {
+        int n=array.length;
+        int min=array[0];
+        for(int i=0;i<n;i++)
+        {if(min>array[i]) min=array[i];}
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+        return min;
     }
 
     /**
      * Chương trình tính chỉ số BMI và in ra kết quả đánh giá
+     *
      * @param weight cân nặng
      * @param height chiều cao
      * @return Thiếu cân, Bình thường, Thừa cân, Béo phì
      */
     public static String calculateBMI(double weight, double height) {
+        String a=" ";
+        double BMI = ((weight)/(height)/(height));
+      if(BMI<18.5)  a="Thiếu cân";
+      else if(BMI>18.5&&BMI<22.99) a="Bình thường";
+      else if(BMI>23&&BMI<24.99) a="Thừa cân";
+      else if(BMI>25) a="Béo phì";
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+          return a;
+    }
+
+    public static void main(String[] a)
+    {
+      int abc[]={1,2,0,3,4,5};
+      System.out.println( calculateBMI(60,1.7));
     }
 }
--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,39 @@
 package week3;
-
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class Week3Test {
-
+    @Test
+ public void testmax()
+ {
+     assertEquals(3,Week3.max(2,3));
+     assertEquals(9,Week3.max(9,4));
+     assertEquals(100,Week3.max(100,1));
+     assertEquals(1000000,Week3.max(0,1000000));
+     assertEquals(1000,Week3.max(1000,999));
+ }
     // TODO: Viết 5 testcase cho phương thức max()
-
+   @Test
+    public void testminarray()
+    {int a[]={3,2,1,4,2,3,5,4,5};
+     int b[]={0};
+     int c[]={1,3,4,5,64,6,342,65,345,4};
+     int d[]={100000000,99};
+     int e[]={1234567,12345678,123456789};
+        assertEquals(1,Week3.minOfArray(a));
+        assertEquals(0,Week3.minOfArray(b));
+        assertEquals(1,Week3.minOfArray(c));
+        assertEquals(99,Week3.minOfArray(d));
+        assertEquals(1234567,Week3.minOfArray(e));
+    }
     // TODO: Viết 5 testcase cho phương thức minOfArray()
-
+@Test
+    public void testBMI()
+    {
+        assertEquals("Thiếu cân",Week3.calculateBMI(54,1.8));
+        assertEquals("Bình thường",Week3.calculateBMI(60,1.7));
+        assertEquals("Thừa cân",Week3.calculateBMI(70,1.7));
+        assertEquals("Béo phì",Week3.calculateBMI(70.55,1.6));
+        assertEquals("Thiếu cân",Week3.calculateBMI(44,1.56));
+    }
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
 }
