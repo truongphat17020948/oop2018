aba763b2c18f5d256f0f5fa12b6a7f4401d132cc
truongphat17020948 <truongphat0799@gmail.com>
2018-10-10 11:19:58 +0700

    update4

--- src/week4/task2/Circle.java
+++ src/week4/task2/Circle.java
@@ -34,4 +34,10 @@ package week4.task2;
     {
         return getArea()+" "+getPerimeter()+" "+getColor()+" "+isFilled();
     }
+     public static void main(String[] argv)
+     {
+         Circle a =new Circle();
+         a.Circle(2,"xanh",true);
+         System.out.println("diện tích chu vi màu sắc và độ lấp đầy màu sắc là: "+a.toString());
+     }
 }
