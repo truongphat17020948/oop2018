feb9ce419b16e9160dab97b361fa6c0bc4a66667
truongphat17020948 <truongphat0799@gmail.com>
2018-10-10 11:18:03 +0700

    update4

--- src/week4/task1/camsanh.java
+++ src/week4/task1/camsanh.java
@@ -23,4 +23,16 @@ package week4.task1;
             System.out.println("ăn tạm");
         }
     }
+     public static void main(String [] arg)
+     {
+         quatao c= new camsanh();
+         hoaqua a = new camsanh();
+         quacam b = new camsanh();
+         quacamphong d=new camsanh();
+         camsanh e = new camsanh();
+         //  c.setPhanloai("táo mĩ");
+        //   c.giatri();
+         //  e.setVi("chua");
+        //   e.danhgia();
+     }
 }
